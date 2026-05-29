d7475ad50d482d6bcaa56a980ae3c51cd5bdab02
Stephen Olasunkanmi <bonniedevis727@gmail.com>
2026-05-29 08:57:01 +0100

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useMemo } from "react";
-import Hero from "@/components/backgrounds/Hero";
 import MediaPillars from "@/components/MediaPillars";
 import PhotoMarquee from "@/components/PhotoMarquee";
 import VisualCTA from "@/components/VisualCTA";
@@ -10,6 +9,7 @@ import { homeSections, visuals } from "@/data/content";
 import getRandomBackground from "@/components/backgrounds";
 import Link from "next/link";
 import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   const { highlight } = homeSections;
